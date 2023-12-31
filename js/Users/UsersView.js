4fa8109276f19b1bbacb8c2e64195a18dd878b55
Anton Kotorovych <akotorovych@gmail.com>
2023-12-31 02:24:20 +0200

    fix englishLevel change listener bug

--- js/Users/UsersView.js
+++ js/Users/UsersView.js
@@ -90,7 +90,7 @@ export default class UsersView {
   }
 
   bindEnglishLevel(handler) {
-    this.englishLevelInput.addEventListener('click', () => {
+    this.englishLevelInput.addEventListener('change', () => {
       handler(this.englishLevelInput.value);
     });
   }
